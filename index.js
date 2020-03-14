a61c977461d1c5b0ba5bfe2207e16389d3e87ee8
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-03-14 16:45:24 +0100

    do it right

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 'use strict';
 
-function makeSwcPlugin(swcOptions) {
-  if (!swcOptions) {
-    swcOptions = {
-      "jsc": {
-        "parser": {
-          "syntax": "ecmascript",
-          "jsx": true,
-          "dynamicImport": false,
-          "numericSeparator": false,
-          "classPrivateProperty": false,
-          "privateMethod": false,
-          "classProperty": false,
-          "functionBind": false,
-          "exportDefaultFrom": false,
-          "exportNamespaceFrom": false,
-          "decorators": false,
-          "decoratorsBeforeExport": false,
-          "nullishCoalescing": false,
-          "importMeta": false,
-          "optionalChaining": false
-        }
-      }
-    };
-
-    return function modify(defaultConfig, { target, dev }, webpack) {
-      const config = defaultConfig;
-
-      config.module.rules = config.module.rules.reduce((rules, rule) => {
-
-        if (rule.test &&
-          rule.test.toString()===/\.(js|jsx|mjs)$/.toString() &&
-          !rule.enforce) { // !rule.enforce to not conflict with eslint plugin
-
-          const { use, ...rest } = rule;
-
-          rules.push({ ...rest, ...{
-            use: [{
-                loader: require.resolve('swc-loader'),
-                options: swcOptions
-              }
-            ]
-          }});
-        }
-        else {
-          rules.push(rule);
-        }
-        return rules;
-      }, []);
-
-      return config;
+const defaultOptions = {
+  "jsc": {
+    "parser": {
+      "syntax": "ecmascript",
+      "jsx": true,
+      "dynamicImport": false,
+      "numericSeparator": false,
+      "classPrivateProperty": false,
+      "privateMethod": false,
+      "classProperty": false,
+      "functionBind": false,
+      "exportDefaultFrom": false,
+      "exportNamespaceFrom": false,
+      "decorators": false,
+      "decoratorsBeforeExport": false,
+      "nullishCoalescing": false,
+      "importMeta": false,
+      "optionalChaining": false
     }
   }
-}
+};
 
-module.exports = makeSwcPlugin();
-module.exports.makeSwcPlugin = makeSwcPlugin;
+module.exports = (
+  defaultConfig,
+  { target, dev },
+  webpack,
+  userOptions = {}
+) => {
+  const config = Object.assign({}, defaultConfig);
+
+  const options = Object.assign({}, defaultOptions, userOptions);
+
+  config.module.rules = config.module.rules.reduce((rules, rule) => {
+
+    if (rule.test &&
+      rule.test.toString()===/\.(js|jsx|mjs)$/.toString() &&
+      !rule.enforce) { // !rule.enforce to not conflict with eslint plugin
+
+      const { use, ...rest } = rule;
+
+      rules.push({ ...rest, ...{
+        use: [{
+            loader: require.resolve('swc-loader'),
+            options: options
+          }
+        ]
+      }});
+    }
+    else {
+      rules.push(rule);
+    }
+    return rules;
+  }, []);
+
+  return config;
+}
